b70ae8d5240a72bac4a512a1be4a0b44b9884921
Andrew Matthews <andrew.matthews@team.telstra.com.au>
2018-12-17 12:11:03 +1100

    impl for creating topics

--- init/rc1.d/01-functions.fish
+++ init/rc1.d/01-functions.fish
@@ -6,8 +6,12 @@ function kafka
     return
   end
   switch $argv[1]
+
     case topics
         kafka_topics 
+
+    case topic
+        kafka_topic $argv[2] $argv[3] $argv[4]
 
     case help
         kafka_help
@@ -27,6 +31,10 @@ function kafka_help -d "display usage info"
   echo "kafka <command> [options] [args]"
   echo ""
   
+  echo "kafka topic name replication_factor partitions"
+  echo "  create a new topic"
+  echo ""
+
   echo "kafka topics"
   echo "  get a list of the topics in the kafka broker"
   echo ""
@@ -42,6 +50,14 @@ function kafka_topics
   set zk_pod_name (ssh $KAFKA_JUMP_HOST kubectl get pods -n $KAFKA_K8S_NS | grep $KAFKA_K8S_ZK_NAME | cut -d' ' -f 1|head -n 1);
   set zk_pod_ip (ssh $KAFKA_JUMP_HOST kubectl get pod $zk_pod_name -n  $KAFKA_K8S_NS -o json | jq -r '.status.podIP');
   set cmd "\"unset JMX_PORT && kafka-topics.sh --zookeeper $zk_pod_ip:2181 --list\"";
+  ssh $KAFKA_JUMP_HOST kubectl -n $KAFKA_K8S_NS exec $pod_name -c kafka -- /bin/bash -c $cmd
+end
+
+function kafka_topic -a topic_name replication_factor partitions
+  set pod_name (ssh $KAFKA_JUMP_HOST kubectl get pods -n $KAFKA_K8S_NS | grep $KAFKA_K8S_NAME | cut -d' ' -f 1|head -n 1);
+  set zk_pod_name (ssh $KAFKA_JUMP_HOST kubectl get pods -n $KAFKA_K8S_NS | grep $KAFKA_K8S_ZK_NAME | cut -d' ' -f 1|head -n 1);
+  set zk_pod_ip (ssh $KAFKA_JUMP_HOST kubectl get pod $zk_pod_name -n  $KAFKA_K8S_NS -o json | jq -r '.status.podIP');
+  set cmd "\"unset JMX_PORT && kafka-topics.sh --create --zookeeper $zk_pod_ip:2181 --replication-factor $replication_factor --partitions $partitions --topic $topic_name\"";
   ssh $KAFKA_JUMP_HOST kubectl -n $KAFKA_K8S_NS exec $pod_name -c kafka -- /bin/bash -c $cmd
 end
 
--- init/rc2.d/01-completion.fish
+++ init/rc2.d/01-completion.fish
@@ -1,2 +1,3 @@
+complete -c kafka -x -a topic -d 'create a new topic'
 complete -c kafka -x -a topics -d 'list all topics'
 complete -c kafka -x -a help  -d 'list all commands'
